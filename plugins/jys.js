1ae00c5c94410671a18678367802f635a2eaf2a4
Arullaja <upisupriatin9@gmail.com>
2022-04-10 22:19:54 +0700

    BotMd

---
+++ plugins/jys.js
@@ -0,0 +1,50 @@
+
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async (m, { command }) => {
+bear = "Source Code"
+ye = `@${m.sender.split`@`[0]}`
+let esce = `
+Hai ${ye} Lagi Nyari Sc Ya? 
+
+udah 2022 masih ngemis esce:v
+
+_*Cari Sendiri aja ya:v*_`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: esce,
+           locationMessage: { 
+           jpegThumbnail: await (await fetch(fla + bear)).buffer() },           
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '📍Instagram',
+               url: instagram
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: '𝙱𝙰𝙲𝙺 𝚃𝙾 𝙼𝙴𝙽𝚄',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+handler.help = ['sc', 'sourcecode']
+handler.tags = ['info']
+handler.command = /^(sc|sourcecode)$/i
+
+module.exports = handler
