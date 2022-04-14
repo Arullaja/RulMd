36de965f3897ffadadb7e5d4d6969bd576fa3f02
Arullaja <89208273+Arullaja@users.noreply.github.com>
2022-04-14 12:15:04 +0700

    Update payment.js

--- plugins/payment.js
+++ plugins/payment.js
@@ -12,12 +12,12 @@ let handler = async (m, { conn, text }) => {
 ╰═ ┅ ═══════
 ▌│█║▌║▌║║▌║▌║█│▌
 `.trim()
-conn.send3Button(m.chat, await (await fetch(fla + 'Payment')).buffer(), ext, `${wm}`, 'Nomor Owner', '.owner', 'Buy Premium', '.premium', 'Sewa Bot', '.sewa', m)
+conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Payment')).buffer(), ext, `${wm}`, 'Nomor Owner', '.owner', 'Buy Premium', '.premium', 'Sewa Bot', '.sewa', m)
 
 }
 handler.help = ['payment']
 handler.tags = ['info']
-handler.command = /^(payment)$/i
+handler.command = /^(payment|pay)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -31,4 +31,4 @@ handler.fail = null
 
 module.exports = handler
 
-let wm = global.botwm
+let wm = global.wm
